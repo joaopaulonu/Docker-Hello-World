2d921d6184884238982d47aa7ba55ab49a96621e
João Paulo Nunes Andrade <142180950+joaopaulonu@users.noreply.github.com>
2026-06-12 12:52:04 -0300

    Add Docker hello-world command output

---
+++ Hello from Docker.cmd
@@ -0,0 +1,10 @@
+C:\\Users\\jpnun> docker run hello-world
+Unable to find image 'hello-world:latest' locally
+latest: Pulling from library/hello-world
+4f55086f7dd0: Pull complete
+Digest: sha256:96498ffd522e70807ab6384a5c0485a79b9c7c08ca79ba08623edcad154e62d
+Status: Downloaded newer image for hello-world:latest
+
+Hello from Docker!
+This message shows that your installation appears to be working correctly.
+...
